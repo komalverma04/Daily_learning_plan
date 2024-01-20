ab830acfc52923430f40f6df3328a203b06623ab
komalverma04 <114138604+komalverma04@users.noreply.github.com>
2024-01-21 00:06:44 +0530

    Create Basic.js
    Practiced JavaScript Basics

---
+++ Day 8/Basic.js
@@ -0,0 +1,17 @@
+//var my_name = "Komal";
+//var your_name = prompt("What is yor name");
+//alert("Hi " + your_name + ", This is "+ my_name + ".");
+
+//var msg = prompt(" Enter a message ");
+//var len_msg = msg.length;
+//alert("You have written " + len_msg + "characters. And you are left with " + (50 - len_msg) + "characters." );
+// SLICING OF STRINGS
+//var myname = "Komal";
+//
+//console.log(myname.slice(0,1));// i want slice of name from position 0 to 1 but not including 1
+//var user_name = prompt("Enter your name");
+//alert("Hi " + (user_name.slice(0,1).toUpperCase()) + (user_name.slice(1,user_name.length)) + " !!");
+// Operators
+var age = prompt("Enter your dog's age");
+var human_age = (age-2)*4 + 21;
+alert("Your dog's age is " + age + " and your age is " + human_age +" accordingly.");
